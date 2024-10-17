0f4212e20fda2a87b66c011048c0d2611c3b89ef
jaehyuk7 <jahyuck13@naver.com>
2024-10-17 09:07:33 +0900

    first commit

---
+++ src/main/java/com/market/studyemail/StudyemailApplication.java
@@ -0,0 +1,13 @@
+package com.market.studyemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyemailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyemailApplication.class, args);
+	}
+
+}
